9fbefc82d9b104378a56745602b8e477ca1ed02f
azujuuuuuun <azujuuuuuun@gmail.com>
2020-06-25 00:09:17 +0900

    Turn @typescript-eslint/explicit-module-boundary-types off

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,5 +23,6 @@ module.exports = {
     "linebreak-style": ["error", "unix"],
     quotes: ["error", "double"],
     semi: ["error", "always"],
+    "@typescript-eslint/explicit-module-boundary-types": "off",
   },
 };
